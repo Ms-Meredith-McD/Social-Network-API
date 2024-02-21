fb333f05d4eb54bd5a5391aeeb8903b6a780ff2e
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-20 20:02:59 -0600

    decided I didn't need as many route files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const reactionRoutes = require("./reaction.routes");
 const thoughtRoutes = require("./thought.routes");
 const userRoutes = require("./user.routes");
 
 
-router.use("/reaction", reactionRoutes);
 router.use("/thought", thoughtRoutes);
 router.use("/user", userRoutes);
 
